ff7a21b26570445ea279e88547b7fb8ee9631dc4
ShamimHossain1 <shamimweb78@gmail.com>
2024-06-03 02:01:45 -0400

    nested fuction

--- class.cpp
+++ class.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 class employee
 {
-    private: 
-        int a, b, c;
+private:
+    int a, b, c;
 
-    public:
-        int d, e;
-        void setdata(int A, int B, int C);
-        void getdata()
-        {
-            cout << "the value of a is " << a << endl;
-            cout << "the value of b is " << b << endl;
-            cout << "the value of c is " << c << endl;
-            cout << "the value of c is " << d << endl;
-            cout << "the value of c is " << e << endl;
-        }
+public:
+    int d, e;
+    void setData(int A, int B, int C);
+    void getData()
+    {
+        cout << "the value of a is " << a << endl;
+        cout << "the value of b is " << b << endl;
+        cout << "the value of c is " << c << endl;
+        cout << "the value of c is " << d << endl;
+        cout << "the value of c is " << e << endl;
+    }
 };
 
-void employee ::setdata(int A, int B, int C)
+void employee ::setData(int A, int B, int C)
 {
     a = A;
     b = B;
@@ -31,12 +31,19 @@ int main()
 {
 
     employee shamim;
+    employee beast;
+
+    beast.d = 59;
+    beast.e = 50;
+
+    beast.setData(10, 405, 340);
+    beast.getData();
 
     shamim.d = 30;
     shamim.e = 40;
 
-    shamim.setdata(1, 2, 3);
-    shamim.getdata();
+    shamim.setData(1, 2, 3);
+    shamim.getData();
 
     return 0;
 }
--- object.cpp
+++ object.cpp
@@ -2,7 +2,43 @@
 
 using namespace std;
 
+ class binary {
+        string s;
+        public:
+         void read(void);
+         void check(void);
+
+    };
+
+    void binary :: read(void){
+
+        cout<<"Enter a Binary number:";
+        cin>>s;
+        
+    }
+    
+    void binary :: check(void){
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.at(i) != '0' && s.at(i) != '1')
+            {
+                cout<<"Invalid Binary Number";
+                exit(0);
+            }
+           
+               
+           
+        }
+         cout<<"Valid Binary Number";
+        
+    }
+
 int main(){
+     
+     binary a;
+     a.read();
+     a.check();
+   
 
     
 
