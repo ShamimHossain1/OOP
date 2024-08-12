4f9297dbd1332ddd1e8b8239dae057150b0846fa
ShamimHossain1 <shamimweb78@gmail.com>
2024-08-12 02:39:53 -0400

    Polymorphism

---
+++ Polymorphism.cpp
@@ -0,0 +1,42 @@
+// Polymorphism
+
+#include<iostream>
+
+using namespace std;
+
+class Base{
+    public:
+    int varBase;
+    void display(){
+        cout<<"Base class var: "<<varBase<<endl;
+    }
+};
+class derived : public Base{
+    public:
+    int varDerived;
+    void display(){
+        cout<<"Derived class var: "<<varDerived<<endl;
+        cout<<"Derived class var: "<<varDerived<<endl;
+    }
+};
+
+int main(){
+
+    Base *Base_pointer;
+    derived *Derived_pointer;
+
+    Base objBase;
+    derived objDerived;
+
+    Base_pointer = &objDerived;
+    Derived_pointer = &objDerived;
+
+    Base_pointer->varBase= 50;
+    Base_pointer->display();
+
+    Derived_pointer->varDerived= 30;
+    Derived_pointer->display();
+    
+
+    return 0;
+}
