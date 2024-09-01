561e146d08ee09e9011d3dfe07adff59931043a9
ShamimHossain1 <shamimweb78@gmail.com>
2024-08-31 20:34:20 -0400

    fibonacci

--- test.cpp
+++ test.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
-#include <cstdio>
 using namespace std;
 
-/*
-Add `int max_of_four(int a, int b, int c, int d)` here.
-*/
-
-int max_of_four(int a, int b, int c, int d)
-{
-
-    int nums[4];
-    nums[0] = a;
-    nums[1] = b;
-    nums[2] = c;
-    nums[3] = d;
-
-    int great = 0;
-
-    for (int i = 0; i < 4; i++)
-    {
-        if (nums[i] > great)
-        {
-            great = nums[i];
-        }
+unsigned long long fibonacci(int n) {
+    if (n < 2) {
+        return n;
     }
-    return great;
+    
+    unsigned long long a = 0, b = 1, c;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
-    int main()
-    {
-        int a, b, c, d;
-        scanf("%d %d %d %d", &a, &b, &c, &d);
-        int ans = max_of_four(a, b, c, d);
-        printf("%d", ans);
 
-        return 0;
-    }
+int main() {
+    cout << "The Fibonacci number at position 60 is: " << fibonacci(60) << endl;
+    return 0;
+}
