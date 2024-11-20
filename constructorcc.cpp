0a4002d2130050f7e55d7d29655f0cf12fc94e3f
ShamimHossain1 <shamimweb78@gmail.com>
2024-11-19 23:39:49 -0500

    constract

---
+++ constructorcc.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Complex
+{
+private:
+    int real;
+
+public:
+    Complex(int param);
+    void Print(void)
+    {
+        cout << "The complex number is " << real << "i" << endl;
+    }
+};
+
+Complex ::Complex(int param)
+{
+    real = param;
+} 
+
+int main()
+{
+
+    Complex c1(10);
+    c1.Print();
+
+    return 0;
+}
