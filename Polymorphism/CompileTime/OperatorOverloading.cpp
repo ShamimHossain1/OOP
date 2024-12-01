87049fe7d6b9c8e2bbd26130a600c55149fdd74d
ShamimHossain1 <shamimweb78@gmail.com>
2024-12-01 00:13:48 -0500

    Function overloading

---
+++ Polymorphism/CompileTime/OperatorOverloading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    double real;
+    double imag;
+
+public:
+    // Constructor
+    Complex(double r = 0, double i = 0) : real(r), imag(i) {}
+
+    // Overload + operator
+    Complex operator+(const Complex& other) {
+        return Complex(real + other.real, imag + other.imag);
+    }
+
+    // Display function
+    void display() const {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(3.5, 2.5);
+    Complex c2(1.5, 4.5);
+
+    Complex c3 = c1 + c2; // Uses overloaded + operator
+    c3.display();
+
+    return 0;
+}
