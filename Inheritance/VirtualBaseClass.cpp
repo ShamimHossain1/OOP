83e625a96e233a8f55a444a8bd3ef787d393c43a
ShamimHossain1 <shamimweb78@gmail.com>
2024-12-04 23:41:51 -0500

    file

--- Inheritance/VirtualBaseClass.cpp
+++ Inheritance/VirtualBaseClass.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 // Virtual Base Class use in------------ Hybrid Inheritance-------------
--- file_In_C++/file1.cpp
+++ file_In_C++/file1.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
+#include<fstream>
+
 
 using namespace std;
 
 int main(){
 
-    
+
+    ofstream out("FileText.txt");
 
     return 0;
 }
