81e818d4e456b8c9cf4b7dd367cc4e711c6b36a1
ShamimHossain1 <shamimweb78@gmail.com>
2024-12-05 01:22:56 -0500

    file read write

--- file_In_C++/file1.cpp
+++ file_In_C++/file1.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 #include<fstream>
-
+#include <string>
 
 using namespace std;
 
 int main(){
+    string st = "Shamim Hossain";
+    string st2;
 
+    // Opening file using constructor and writing in it
+    // ofstream out("FileText.txt");
+    // out<<st;
 
-    ofstream out("FileText.txt");
+    // Opening file using constructor and reading it
+    ifstream in("FileText.txt");
+    // in>>st2;
+   getline(in, st2);
+    cout<<st2;
+
 
     return 0;
 }
